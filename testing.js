93872367d7cf4e48dc4adb7b50ef1ea74588951c
raporpe <raporpe@gmail.com>
2020-10-09 13:49:51 +0200

    Finished algorithm

--- testing.js
+++ testing.js
@@ -1,4 +1,7 @@
 var numberOfDivisions = 5;
+//var nSeed = Math.floor(Math.random()*100000000000000);
+//var cSeed = Math.floor(Math.random()*100000000000000);
+
 var nSeed = 42;
 var cSeed = 42;
 
@@ -7,19 +10,29 @@ function setup() {
     background(100);
     t = 0;
     ix = 0;
-    init();
-    randomSeed(cs);
-    noiseSeed(nSeed)
+    randomSeed(cSeed);
+    noiseSeed(nSeed);
+    console.log(cSeed);
+    console.log(nSeed);
+    console.log("..");
 }
 
 
 function init() {
 
     colorMode(HSB, 1);
-    col0 = color(random(), 1 - sq(random()), 1 - sq(random()));
-    col1 = color(random(), 1 - sq(random()), 1 - sq(random()));
-    col2 = color(random(), 1 - sq(random()), 1 - sq(random()));
-    col3 = color(random(), 1 - sq(random()), 1 - sq(random()));
+
+    col0 = color(random(), 1 - sq(random())+0.07, 1 - sq(random())+0.07);
+    col1 = color(random(), 1 - sq(random())+0.07, 1 - sq(random())+0.07);
+    col2 = color(random(), 1 - sq(random())+0.07, 1 - sq(random())+0.07);
+    col3 = color(random(), 1 - sq(random())+0.07, 1 - sq(random())+0.07);
+
+    // My own colors
+
+    // col0 = color(126, 1 - (random()), 1 - (random()));
+    // col1 = color(random(), 1 - (random()), 1 - (random()));
+    // col2 = color(random(), 1 - (random()), 1 - (random()));
+    // col3 = color(random(), 1 - (random()), 1 - (random()));
 
     // Copy values
     ncol0 = col0;
@@ -29,22 +42,18 @@ function init() {
 
     colorMode(RGB, 255);
 
-            random(50, 450);
+    // Resolution division factor
     res1 = 150 - random(100) + random(300); 
     res2 = 150 - random(100) + random(300);
     res0 = 150 - random(100) + random(300);
     res = 150 - random(100) + random(300);
 
-    shp0 = 3 + random(7);
-    shp1 = 3 + random(7);
-    shp2 = 3 + random(7);
-
     str0 = 20 - random(10) + random(10);
     str1 = 20 - random(10) + random(10);
     str2 = 20 - random(10) + random(10);
 
-    // Distortioners
-    disstr = random(1) - random(.8) + random(1.5);
+    // Distortioners for the third dimmension
+    disstr = random(1) - random(.8) + random(1.5); 
     disstr0 = 5 - random(3) + random(5);
     disstr1 = 5 - random(3) + random(5);
     disstr2 = 5 - random(3) + random(5);
@@ -54,29 +63,34 @@ function init() {
 
 function draw() {
 
+    // Check if finished drawing
+    if (ix > windowWidth) {
+        saveCanvas("Dream " + (t++), "png");
+        noLoop();
+    }
+
+    // Check if the zone was finished
+    if (ix % (floor(windowWidth / numberOfDivisions)) == 0){
+        init();
+    } 
+
     // Render vertical line
     for (iy = 0; iy < windowHeight; iy++) {
         v = sq(noise(ix / res, iy / res)) * disstr;
-        //,0.005*sqrt(sq(ix-windowWidth/2)+sq(iy-windowHeight/2))
         v0 = noise(ix / res0 + 10, iy / res0, v * disstr0 + 4);
         v1 = noise(ix / res1, iy / res1 + 10, v * disstr1 + 14);
         v2 = noise(ix / res2 + 10, iy / res2 + 10, v * disstr2 + 24);
 
-        /*if (abs(noise(ix/500,iy/500)) > .55)
-        {
-            v0 = 1-v0
-            v1 = 1-v1
-            v2 = 1-v2
-        }*/
 
         v0 = v0 + sin(v0 * 50) / 65;
         v1 = v1 + sin(v1 * 50) / 65;
         v2 = v2 + sin(v2 * 50) / 65;
 
-        ncol0 = lerpColor(col0, color(0, 0, 0), (ix + iy) % 8 < 3 ? .03 : 0);
-        ncol2 = lerpColor(col2, color(255, 255, 255), (ix - iy + 10000) % 8 < 3 ? .03 : 0);
-        ncol1 = lerpColor(col1, color(64, 64, 64), (ix + iy) % 5 < 2 ? .03 : 0);
-        ncol3 = lerpColor(col3, color(192, 192, 192), (ix - iy + 10000) % 5 < 2 ? .03 : 0);
+        // This adds the strange diagonal artifacts
+        //ncol0 = lerpColor(col0, color(0, 0, 0), (ix + iy) % 8 < 3 ? .03 : 0);
+        //ncol2 = lerpColor(col2, color(255, 255, 255), (ix + iy) % 8 < 3 ? .03 : 0);
+        //ncol1 = lerpColor(col1, color(64, 64, 64), (ix + iy) % 5 < 2 ? .03 : 0);
+        //ncol3 = lerpColor(col3, color(192, 192, 192), (ix + iy) % 5 < 2 ? .03 : 0);
 
         col01 = lerpColor(ncol0, ncol1, 1 / (1 + exp((v0 - .5) * str0)));
         col23 = lerpColor(ncol2, ncol3, 1 / (1 + exp((v1 - .5) * str1)));
@@ -86,12 +100,7 @@ function draw() {
         stroke(col);
         point(ix, iy);
     }
-    if (ix % (windowWidth / numberOfDivisions) == 0) init();
     ix++;
 
 
-    if (ix > windowWidth) {
-        saveCanvas("Dream " + (t++), "png");
-        noLoop();
-    }
 }
